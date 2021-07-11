9eed9f456ad4c39a4dc6c63f21f3d723c7e52d16
Kirill <kmakarov@i-neti.ru>
2021-07-11 19:06:51 +0300

    add new answer version

--- bot.py
+++ bot.py
@@ -17,11 +17,65 @@ def welcome(message):
 
     markup.add(item1, item2)
 
-    bot.send_message(message.chat.id, 'Добро пожаловать, {0.first_name}'.format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
+    bot.send_message(message.chat.id,"Добро пожаловать, {0.first_name}!".format(
+                         message.from_user, bot.get_me()),
+                     parse_mode='html', reply_markup=markup)
 
-# @bot.message_handler(content_types=['text'])
-# def m(message):
-#     bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types=['text'])
+def m(message):
+    if message.chat.type == 'private':
+        if message.text == '🤵 Связаться с администратором':
+            bot.send_message(message.chat.id, 'Администратор ответит вам в течении 5 минут')
+        elif message.text == '🕵️‍♀ Посмотреть товар':
+            markup = types.InlineKeyboardMarkup()
+
+            markup.row(types.InlineKeyboardButton("Испарители", callback_data='evaporator'))
+            markup.row(types.InlineKeyboardButton("Фирменные табаки", callback_data='tobacco'))
+            markup.row(types.InlineKeyboardButton("Зажигалки", callback_data='lighter'))
+
+            bot.send_message(message.chat.id, 'Что именно вас интересует?', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Я не знаю что ответить 😢')
+    bot.send_message(message.chat.id, message.text)
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_inline(call):
+        try:
+            if call.message:
+                if call.data == 'evaporator':
+                    markup = types.InlineKeyboardMarkup(row_width=3)
+                    item1 = types.InlineKeyboardButton("Испарители марка 1", callback_data='evaporator1')
+                    item2 = types.InlineKeyboardButton("Испарители марка 2", callback_data='evaporator2')
+                    item3 = types.InlineKeyboardButton("Испарители марка 3", callback_data='evaporator3')
+
+                    markup.add(item1, item2, item3)
+
+                    bot.send_message(message.chat.id, 'Выберите нужный испаритель?', reply_markup=markup)
+                elif call.data == 'tobacco':
+
+                    markup = types.InlineKeyboardMarkup(row_width=3)
+                    item1 = types.InlineKeyboardButton("Табак марка 1", callback_data='tobacco1')
+                    item2 = types.InlineKeyboardButton("Табак марка 2", callback_data='tobacco2')
+                    item3 = types.InlineKeyboardButton("Табак марка 3", callback_data='tobacco3')
+
+                    markup.add(item1, item2, item3)
+
+                    bot.send_message(message.chat.id, 'Выберите нужный табак?', reply_markup=markup)
+
+                elif call.data == 'lighter':
+
+                    markup = types.InlineKeyboardMarkup(row_width=3)
+                    item1 = types.InlineKeyboardButton("Зажигалка 1", callback_data='lighter1')
+                    item2 = types.InlineKeyboardButton("Зажигалка 2", callback_data='lighter2')
+                    item3 = types.InlineKeyboardButton("Зажигалка 3", callback_data='lighter3')
+
+                    markup.add(item1, item2, item3)
+
+                    bot.send_message(message.chat.id, 'Выберите нужную зажигалку?', reply_markup=markup)
+
+
+        except Exception as e:
+            print(repr(e))
 
 #RUN
 bot.polling(none_stop=True)
